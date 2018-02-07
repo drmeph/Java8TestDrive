be2ec6f10ab0206e72525c77ddef4a8ececfeb18
Kevin Dorfer <kevin.dorfer@gmail.com>
2018-02-07 16:18:11 -0500

    added Method Reference Tester

--- src/main/java/com/drmeph/java8testdrive/MethodReferenceTester.java
+++ src/main/java/com/drmeph/java8testdrive/MethodReferenceTester.java
@@ -1,7 +1,30 @@
 package com.drmeph.java8testdrive;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by kevin.dorfer on 2/5/2018.
  */
 public class MethodReferenceTester {
+
+    public static void main(String[] args) {
+        List names = new ArrayList() {{
+            add("David");
+            add("Alexis");
+            add("Michael");
+            add("Rajiv");
+            add("Jeff");
+        }};
+
+        //static method reference
+        names.forEach(System.out::println);
+    }
+
+    /*
+     * TODO
+     * Reference to a constructor
+     * Reference to an instance method of an arbitrary object of a particular type
+     * Reference to an instance method of a particular object
+     */
 }
